56645f592b0b1ee293b180efdad6f58a82b0c8da
james <james@mercstudio.com>
2026-05-28 14:20:31 +0800

    feat(web): add session list, model selector, and agent tabs sidebar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import { ChatProvider } from "./stores/chatStore";
 import ChatPanel from "./components/Chat/ChatPanel";
 import ChatInput from "./components/Chat/ChatInput";
+import SessionList from "./components/Sidebar/SessionList";
+import ModelSelector from "./components/Sidebar/ModelSelector";
+import AgentTabs from "./components/Sidebar/AgentTabs";
 
 export default function App() {
+  const [activeAgent, setActiveAgent] = useState("coder");
+
   return (
     <ChatProvider>
-      <div className="flex h-screen flex-col">
-        <ChatPanel />
-        <ChatInput />
+      <div className="flex h-screen">
+        <aside className="w-64 flex-shrink-0 border-r border-zinc-700 bg-zinc-900 flex flex-col">
+          <SessionList />
+          <ModelSelector />
+          <AgentTabs activeAgent={activeAgent} onSelect={setActiveAgent} />
+        </aside>
+        <main className="flex flex-1 flex-col">
+          <ChatPanel />
+          <ChatInput />
+        </main>
       </div>
     </ChatProvider>
   );
